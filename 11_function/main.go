1392bd98ddfce75034afc212aec6a2484a1b2858
Jinga <info.alpain@gmail.com>
2022-12-14 16:37:27 +0900

    <mod> 関数にスライスを渡す

--- 11_function/main.go
+++ 11_function/main.go
@@ -21,6 +21,10 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func foo(params ...int) {
+	fmt.Println(len(params), params)
+}
+
 func main() {
 	// 関数のコール
 	r := add(10, 20)
@@ -38,4 +42,13 @@ func main() {
 	func(x int) {
 		fmt.Println("inner func", x)
 	}(2)
+
+	// 可変長引数
+	foo()
+	foo(10, 20)
+	foo(10, 20, 30)
+
+	// スライス渡し
+	s := []int{1, 2, 3}
+	foo(s...)
 }
