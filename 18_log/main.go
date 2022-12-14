7bd30521ab94c15733ae28a027a1b98acdd944e6
Jinga <info.alpain@gmail.com>
2022-12-14 20:05:52 +0900

    <mod> サンプル追加

--- 18_log/main.go
+++ 18_log/main.go
@@ -1,9 +1,26 @@
 // lesson18: logの練習
 package main
 
-import "log"
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func LoggingSettings(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
 
 func main() {
 	log.Println("logging!")
 	log.Printf("%T %v", "test", "test")
+
+	LoggingSettings("test.log")
+	_, err := os.Open("fdafdsafa")
+	if err != nil {
+		log.Fatalln("Exit", err)
+	}
 }
--- 19_errorlog/main.go
+++ 19_errorlog/main.go
@@ -4,9 +4,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
+	file, err := os.Open("./main.go")
+	if err != nil {
+		// Fatalfを使うことでerrorlogを出力でき、その後の処理は実行されない
+		log.Fatalln("Error!")
+	}
+	defer file.Close()
+	data := make([]byte, 20)
+	count, err := file.Read(data)
+	if err != nil {
+		// Fatalfを使うことでerrorlogを出力でき、その後の処理は実行されない
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
+
 	// Fatalfを使うことでerrorlogを出力でき、その後の処理は実行されない
 	log.Fatalf("%T %v", "test", "test")
 	fmt.Println("ok?")
