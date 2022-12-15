9798e3f7a25893388b78319c8f4ecd47770619a6
Jinga <info.alpain@gmail.com>
2022-12-15 11:59:25 +0900

    <add> lesson34: channelとselectの複用

---
+++ 34_channel_select/main.go
@@ -0,0 +1,38 @@
+// lesson34: channelとselectの複用
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func goroutine1(ch chan string) {
+	for {
+		ch <- "packet from 1"
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func goroutine2(ch chan int) {
+	for {
+		ch <- 100
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan int)
+	go goroutine1(c1)
+	go goroutine2(c2)
+
+	for {
+		// channelを複数使用する時に利用する。お互いのchannelは干渉しない。
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
+}
