31500a334c4fc305e2ddc5d0910e373264de3dd0
Jinga <info.alpain@gmail.com>
2022-12-15 09:28:45 +0900

    <add> lesson30: Buffered Channelの練習

--- 30_BufferedChannels/main.go
+++ 30_BufferedChannels/main.go
@@ -10,4 +10,20 @@ func main() {
 	fmt.Println(len(ch))
 	ch <- 200
 	fmt.Println(len(ch))
+
+	// Channelからの値取り出し
+	x := <-ch
+	fmt.Println(x)
+	x = <-ch
+	fmt.Println(x)
+
+	// rangeによる取り出し
+	ch <- 300
+	fmt.Println(len(ch))
+	ch <- 400
+	fmt.Println(len(ch))
+	close(ch) // rangeで取り出す場合は一度Channelをcloseする
+	for c := range ch {
+		fmt.Println(c)
+	}
 }
