49f620cf4c97c890063fa11341f8a403294873b4
shota_niga <shota.niga@informatix.co.jp>
2023-01-31 22:27:45 +0900

    <add> initial commit

---
+++ 38_bcrypto/main.go
@@ -0,0 +1,25 @@
+// lesson38: bcryptoによるパスワードのハッシュ値化
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	password := []byte("password")
+
+	// パスワードのハッシュ値化（第2引数はストレッチングの回数（4-31の範囲で指定する））
+	hashed, _ := bcrypt.GenerateFromPassword(password, 10)
+	fmt.Println(string(hashed))
+	// => $2a$10$P9Zr9LES1Yv/n6k77pDy0OVwCRBeHRhHsFMQyU6GfkfpOXfHOPjgG
+
+	// パスワードの検証（正しい場合はnil, 誤っている場合はbcrypt.ErrMismatchedHashAndPasswordを返す）
+	err := bcrypt.CompareHashAndPassword(hashed, password)
+	fmt.Println(err)
+	// => <nil>
+	err = bcrypt.CompareHashAndPassword(hashed, []byte("INCORRECT_PASSWORD"))
+	fmt.Println(err == bcrypt.ErrMismatchedHashAndPassword)
+	// => true
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Jingasan/go_lesson
 
 go 1.18
+
+require golang.org/x/crypto v0.5.0 // indirect
