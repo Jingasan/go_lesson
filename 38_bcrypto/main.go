8a43cad88e70455fce1dceba44d64b24cc80d6b5
shota_niga <shota.niga@informatix.co.jp>
2023-01-31 22:32:17 +0900

    <add> initial commit

--- 38_bcrypto/main.go
+++ 38_bcrypto/main.go
@@ -11,12 +11,15 @@ func main() {
 	password := []byte("password")
 
 	// パスワードのハッシュ値化（第2引数はストレッチングの回数（4-31の範囲で指定する））
-	hashed, _ := bcrypt.GenerateFromPassword(password, 10)
+	hashed, err := bcrypt.GenerateFromPassword(password, 10)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 	fmt.Println(string(hashed))
 	// => $2a$10$P9Zr9LES1Yv/n6k77pDy0OVwCRBeHRhHsFMQyU6GfkfpOXfHOPjgG
 
 	// パスワードの検証（正しい場合はnil, 誤っている場合はbcrypt.ErrMismatchedHashAndPasswordを返す）
-	err := bcrypt.CompareHashAndPassword(hashed, password)
+	err = bcrypt.CompareHashAndPassword(hashed, password)
 	fmt.Println(err)
 	// => <nil>
 	err = bcrypt.CompareHashAndPassword(hashed, []byte("INCORRECT_PASSWORD"))
